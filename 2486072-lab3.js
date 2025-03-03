e00fde9e4ec18eefe172e79a538af7ae72245e84
hulkingpluto <160408011+hulkingpluto@users.noreply.github.com>
2025-03-03 15:35:18 +0200

    Add files via upload

---
+++ 2486072-lab3.js
@@ -0,0 +1,26 @@
+function getMusicTitlesByYear(music_tracks){
+    let track={};
+    music_tracks.map(item =>{
+        if(track[item.year] != undefined){
+            const correct = track[item.year];
+            correct.push(item.title);
+        }
+        else if(track[item.year] != int){
+            console.log('year is not a number')
+        }
+        else{
+            track[item.year]=[item.title];
+        }
+    });
+    return track;
+
+}
+
+const tracks = [
+    { title: 'Blinding Lights', artist: 'The Weeknd', year: 2020 },
+    { title: 'Levitating', artist: 'Dua Lipa', year: 2021 },
+    { title: 'Save Your Tears', artist: 'The Weeknd', year: 2020 },
+  ];
+
+  console.log(getMusicTitlesByYear(tracks));
+
